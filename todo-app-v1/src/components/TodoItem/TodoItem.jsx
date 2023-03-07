21a2caf20ad683b85ac27c36902c815b15ffaf52
GaranceSan <garancesan@gmail.com>
2023-03-07 12:15:39 +0100

    show created as date on ui

--- todo-app-v1/src/components/TodoItem/TodoItem.jsx
+++ todo-app-v1/src/components/TodoItem/TodoItem.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { FiTrash } from "react-icons/fi";
 
 function TodoItem({ todoItem, toggleFn, deleteFn }) {
+  const date = new Date(todoItem.created);
+
   return (
     <li key={todoItem.id}>
       <button onClick={() => toggleFn(todoItem.id)}>
         {todoItem.done ? "done" : "X"}
       </button>
-      <span>{todoItem.created}</span>
+      <span>{date.toDateString()}</span>
       {todoItem.task}
 
       <button onClick={() => deleteFn(todoItem.id)}>
