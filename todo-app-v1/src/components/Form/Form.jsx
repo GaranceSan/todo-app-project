91c172804f37c7cede53a73840acaee739de8956
GaranceSan <garancesan@gmail.com>
2023-03-16 16:06:25 +0100

    Users get now an error message if they enter an empty todo

--- todo-app-v1/src/components/Form/Form.jsx
+++ todo-app-v1/src/components/Form/Form.jsx
@@ -3,18 +3,29 @@ import React from "react";
 
 import styles from "./Form.module.css";
 
-function Form({ handleSubmit }) {
+function Form({ handleSubmit, formErrors, taskFocus }) {
+  console.log("formErrors in Form", formErrors);
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} noValidate>
       <div>
         <label htmlFor="task"> Il faut : </label>
         <input
           id="task"
           name="task"
           type="text"
+          required
           placeholder="ecrire une tache"
+          onFocus={() => taskFocus()}
         />
+        {formErrors.task.length ? (
+          <ul>
+            {formErrors.task.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        ) : null}
       </div>
+
       <div>
         <button className={styles.button} type="submit">
           Add
