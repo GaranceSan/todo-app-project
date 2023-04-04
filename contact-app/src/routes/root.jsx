2bd5d94549b7e848a1abd9c5ccbd57e705fb6475
GaranceSan <garancesan@gmail.com>
2023-04-04 16:08:01 +0200

    Data Loader is installed

--- contact-app/src/routes/root.jsx
+++ contact-app/src/routes/root.jsx
@@ -1,6 +1,8 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData } from "react-router-dom";
+import { getContacts } from "../contacts";
 
 export default function Root() {
+  const { contacts } = useLoaderData();
   return (
     <>
       <div id="sidebar">
@@ -22,14 +24,29 @@ export default function Root() {
           </form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contact.map((contact) => (
+                <li key={contacts.id}>
+                  <Link to={`/contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No name</i>
+                    )}
+                    {""}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="detail">
@@ -37,4 +54,9 @@ export default function Root() {
       </div>
     </>
   );
+}
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
 }
