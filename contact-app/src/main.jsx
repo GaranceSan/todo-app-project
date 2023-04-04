23174a47a63b86335c87295e570c53e79eeddc0f
GaranceSan <garancesan@gmail.com>
2023-04-04 15:09:17 +0200

    Contact Route is created

--- contact-app/src/main.jsx
+++ contact-app/src/main.jsx
@@ -4,11 +4,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
