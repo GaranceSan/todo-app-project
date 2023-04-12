d090fe6ce8bc322458a2f043740f785410464fa5
GaranceSan <garancesan@gmail.com>
2023-04-12 11:09:53 +0200

    General improvement of the app + improvement of the search feature + improvement of the favorite feature

--- contact-app/src/main.jsx
+++ contact-app/src/main.jsx
@@ -9,9 +9,13 @@ import Root, {
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
-import { loader as contactLoader } from "./routes/contact";
+import {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -21,10 +25,12 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
@@ -35,6 +41,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <div>Ooops! une erreur a eu lieu</div>,
       },
     ],
   },
